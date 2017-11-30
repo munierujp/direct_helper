b493e12d8fd3a0eaaba8bb1b0a094a663a4cdf95
Munieru <mail@munieru.jp>
2017-11-30 21:56:45 +0900

    入力文字数の表示形式にカウントアップを追加

--- src/direct_helper.user.js
+++ src/direct_helper.user.js
@@ -358,9 +358,26 @@
 					{
 						key: "show_message_count",
 						name: "入力文字数の表示",
-						description: "入力文字数をカウントダウン形式で表示します。",
+						description: "入力文字数を表示します。",
 						type: FormTypes.CHECKBOX,
 						default: true
+					},
+					{
+						key: "show_message_count_types",
+						name: "入力文字数の表示形式",
+						type: FormTypes.RADIOBUTTON,
+						default: "countdown",
+                        parentKey: "show_message_count",
+                        buttons: [
+                            {
+                                key: "countdown",
+                                name: "カウントダウン"
+                            },
+                            {
+                                key: "countup",
+                                name: "カウントアップ"
+                            }
+                        ]
 					}
 				]
 			},
@@ -968,20 +985,24 @@
     * 入力文字数の表示機能を実行します。
     */
 	function doShowMessageCount(){
+		const settings = getSettings();
+        const countDown = settings.show_message_count_types== "countdown";
+
 		const sendForms = $('.form-send');
 		sendForms.each((i, sendForm) => {
 			const $textArea = $(sendForm).find('.form-send-text');
 			const maxLength = $textArea.prop("maxLength");
 
 			//カウンターを作成
-			const $counter = $(`<label>${maxLength}</label>`).css("margin-right", "8px");
+            const count = countDown ? maxLength : 0;
+			const $counter = $(`<label>${count}</label>`).css("margin-right", "8px");
             const $sendButtonGroup = $(sendForm).find('.form-send-button-group');
 			$sendButtonGroup.prepend($counter);
 
 			//文字入力時にカウンターの値を更新
             $textArea.on("input.direct_helper_doShowMessageCount", () => {
                 const currentLength = $textArea.val().length;
-                const count = maxLength - currentLength;
+                const count = countDown ? maxLength - currentLength : currentLength;
                 $counter.text(count);
             });
 		});

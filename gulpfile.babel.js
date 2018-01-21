bd62e1375fe952cba53eb935b36e656bfc4116a9
Munieru <mail@munieru.jp>
2018-01-22 00:19:59 +0900

    Update gulp config
    Use ES2017 in bable task.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -80,7 +80,7 @@ gulp.task('chromeManifest', () => {
 gulp.task('babel', () => {
   return gulp.src('app/scripts.babel/**/*.js')
       .pipe($.babel({
-        presets: ['es2015']
+        presets: ['es2017']
       }))
       .pipe(gulp.dest('app/scripts'));
 });

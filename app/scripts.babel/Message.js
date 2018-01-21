3b936b5799eecdb8acaa59e92f0d188114c310e1
Munieru <mail@munieru.jp>
2018-01-22 02:14:31 +0900

    Add Message.js

---
+++ app/scripts.babel/Message.js
@@ -0,0 +1,46 @@
+import Talk from './Talk';
+import formatDate from './formatDate';
+
+/** メッセージ */
+class Message{
+  /**
+  * @param {Talk} talk Talkオブジェクト
+  * @throws {TypeError} talkの型がTalkではない場合
+  */
+  constructor(talk){
+    if(!(talk instanceof Talk)){
+      throw new TypeError(talk + ' is not instance of Talk');
+    }
+    this.talk = talk;
+  }
+
+  /**
+  * Messageオブジェクトを生成します。
+  * @param {Talk} talk Talkオブジェクト
+  * @return {Message} Messageオブジェクト
+  */
+  static of(talk){
+    return new this(talk);
+  }
+
+  /**
+  * メッセージをコンソールに出力します。
+  * @param {Object} settings 設定
+  */
+  log(settings){
+    const header = Replacer.of(
+      [/<talkId>/g, this.talk.id],
+      [/<time>/g, formatDate(this.time, settings.date_format)],
+      [/<talkName>/g, this.talk.name],
+      [/<userName>/g, this.userName]
+    ).exec(settings.custom_log_message_header);
+
+    console.group(header);
+    Optional.ofAbsentable(this.stamp)
+      .ifPresent(stamp => console.log(settings.log_label, this.body, stamp))
+      .ifAbsent(() => console.log(settings.log_label, this.body));
+    console.groupEnd();
+  }
+}
+
+export default Message;
--- app/scripts.babel/main.js
+++ app/scripts.babel/main.js
@@ -1,51 +1,11 @@
 import 'babel-polyfill';
 import HasValue from './HasValue';
 import Talk from './Talk';
+import Message from './Message';
+import formatDate from './formatDate';
 
 (function(){
   'use strict';
-
-  /** メッセージ */
-  class Message{
-    /**
-    * @param {Talk} talk Talkオブジェクト
-    * @throws {TypeError} talkの型がTalkではない場合
-    */
-    constructor(talk){
-      if(!(talk instanceof Talk)){
-        throw new TypeError(talk + ' is not instance of Talk');
-      }
-      this.talk = talk;
-    }
-
-    /**
-    * Messageオブジェクトを生成します。
-    * @param {Talk} talk Talkオブジェクト
-    * @return {Message} Messageオブジェクト
-    */
-    static of(talk){
-      return new this(talk);
-    }
-
-    /**
-    * メッセージをコンソールに出力します。
-    * @param {Object} settings 設定
-    */
-    log(settings){
-      const header = Replacer.of(
-        [/<talkId>/g, this.talk.id],
-        [/<time>/g, formatDate(this.time, settings.date_format)],
-        [/<talkName>/g, this.talk.name],
-        [/<userName>/g, this.userName]
-      ).exec(settings.custom_log_message_header);
-
-      console.group(header);
-      Optional.ofAbsentable(this.stamp)
-        .ifPresent(stamp => console.log(settings.log_label, this.body, stamp))
-        .ifAbsent(() => console.log(settings.log_label, this.body));
-      console.groupEnd();
-    }
-  }
 
   /** トークエリア */
   class TalkArea extends HasValue{
@@ -1170,55 +1130,6 @@ import Talk from './Talk';
   */
   function removeBlank(source) {
     return source.replace(/[\s　]/g, '');
-  }
-
-  /**
-  * Dateオブジェクトを指定したパターンでフォーマットします。
-  * cf. https://docs.oracle.com/javase/jp/8/docs/api/java/time/format/DateTimeFormatter.html#patterns
-  * @param {Date} date Dateオブジェクト
-  * @param {String} pattern パターン
-  * @return {String} フォーマットした文字列
-  */
-  function formatDate(date, pattern){
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const dayOfMonth = date.getDate();
-    const dayOfWeek = date.getDay();
-    const dayTexts = ['日', '月', '火', '水', '木', '金', '土'];
-    const dayOfWeekText = dayTexts[dayOfWeek];
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    return Replacer.of(
-      [/yyyy/g, year],
-      [/yy/g, year % 100],
-      [/MM/g, zeroPadding(month, 2)],
-      [/M/g, month],
-      [/dd/g, zeroPadding(dayOfMonth, 2)],
-      [/d/g, dayOfMonth],
-      [/e/g, dayOfWeekText],
-      [/HH/g, zeroPadding(hours, 2)],
-      [/H/g, hours],
-      [/mm/g, zeroPadding(minutes, 2)],
-      [/m/g, minutes],
-      [/ss/g, zeroPadding(seconds, 2)],
-      [/s/g, seconds]
-    ).exec(pattern);
-  }
-
-  /**
-  * 数値を指定した桁数になるようにゼロ埋めします。
-  * @param {Number} num 数値
-  * @param {Number} digits 桁数
-  * @return {String} ゼロ埋めした文字列
-  */
-  function zeroPadding(num, digits){
-    const source = String(num);
-    let zeros = '';
-    for(let i = 0; i < digits - source.length; i++){
-      zeros += '0';
-    }
-    return zeros + source;
   }
 
   /**

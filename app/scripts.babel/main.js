2b7e9c8be5e02884bda456fcd3f36ab970510be8
Munieru <mail@munieru.jp>
2018-01-23 22:03:23 +0900

    Beautify main.js
    Remove comment.

--- app/scripts.babel/main.js
+++ app/scripts.babel/main.js
@@ -7,13 +7,10 @@ import doActions from '@functions/doActions';
 (function(){
   'use strict';
 
-  //設定の初期化
   initializeSettings()
   .then(() => {
-    //設定画面の描画
     drawSettingView();
 
-    //各種機能の実行
     doActions();
   });
 })();
